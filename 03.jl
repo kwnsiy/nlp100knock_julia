6df3e6f15a9458cd285daa3b5ba0b7c42d5145c1
kawa5512yakiudon <sei5512kkk@yahoo.co.jp>
2015-10-26 07:13:53 +0900

    update

--- 03.jl
+++ 03.jl
@@ -2,4 +2,4 @@
 #単語分割・文字数
 str = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
 counts = [length(word) for word in split(str," ")]
-println()
+println(counts)
